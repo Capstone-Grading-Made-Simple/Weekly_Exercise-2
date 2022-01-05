3650cf23b7972c1d91f6cffc5dd0093447f5653d
Tiang Ann Linus Tay <s10195119@connect.np.edu.sg>
2022-01-05 10:37:43 +0800

    Test

--- app/src/main/java/com/example/week2exercise/User.kt
+++ app/src/main/java/com/example/week2exercise/User.kt
@@ -11,7 +11,7 @@ class User (name: String, description: String, id: Int, followed: Boolean) {
     }
 
     fun getName() : String {
-        return name
+        return true
     }
 
     fun getDescription() : String {
