cd9ebcba9b8bc69025437b43c702dd0640e332d7
Tiang Ann Linus Tay <s10195119@connect.np.edu.sg>
2022-01-12 09:07:14 +0800

    Test

--- app/src/main/java/com/example/week2exercise/MainActivity.kt
+++ app/src/main/java/com/example/week2exercise/MainActivity.kt
@@ -8,7 +8,7 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
